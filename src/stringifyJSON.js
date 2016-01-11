7e8f1fa0ae3fa63257dd245d4a93dfb4d1ac934c
Justin Bitely <jbitely@Justins-MacBook-Pro.local>
2016-01-11 18:58:54 -0500

    completed stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,28 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null){
+  	return "null";
+  }
+  if (obj.constructor === Array){
+  	var results = [];
+  	for(var i = 0; i < obj.length; i++){
+  		results.push(stringifyJSON(obj[i]));
+  	}
+  	return '[' + results.join(',') + ']';
+  }
+  if (obj.constructor === Object){
+  	var results = [];
+  	for(var key in obj){
+  		if(typeof obj[key] === 'function' || obj[key] === undefined){
+  			continue;
+  		}
+  		results.push(stringifyJSON(key)+':'+stringifyJSON(obj[key]));
+  	}
+  	return "{" + results.join(',') + "}";
+  }
+  if (obj.constructor === String){
+  	return '"'+obj+'"';
+  }
+  return ''+obj;
 };
